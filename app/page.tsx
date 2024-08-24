9f96f0ad7201acbd9c1cb655af660f0c58a76516
Kenneth Poenadi <19623221@std.stei.itb.ac.id>
2024-08-24 16:15:37 +0700

    fix error with map

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -8,17 +8,19 @@ import { useEffect } from "react";
 const DynamicMapComponent = dynamic(() => import('./components/MapComponent'), { ssr: false });
 
 const Home: React.FC = () => {
+  const { data: session, status } = useSession();
   const router = useRouter();
 
   useEffect(() => {
-    const checkSession = async () => {
-      const session = await getSession();
-      if (!session) {
-        router.push('/auth/signin');
-      }
-    };
-    checkSession();
-  }, [router]);
+    if (status === 'loading') return; // Avoid redirecting while the session is loading
+    if (!session) {
+      router.push('/auth/signin');
+    }
+  }, [session, status, router]);
+
+  if (status === 'loading') {
+    return <p>Loading...</p>; // You can render a loading state here
+  }
 
   return (
     <main style={{ height: '100vh' }}>
